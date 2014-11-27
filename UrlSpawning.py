ec0e4349292a984ffa3ee24b63e9fe8424aba63a
faroncoder <faronledger@gmail.com>
2014-11-27 16:47:33 -0500

    updates via command line

--- UrlSpawning.py
+++ UrlSpawning.py
@@ -5,8 +5,14 @@ class open_browser_url(sublime_plugin.TextCommand):
 
    def run(self,edit):
       url = self.view.file_name()
+
       url = url.replace('\\', '/')
-      url = url.replace( '/home/faron/lib/script/htmls' ,'www.faronintel.ca')
+      url = url.replace('/home/faron/lib/script/htmls/projects','www.faronintel.ca/projects')
+      url = url.replace('/home/faron/lib/script/htmls/deployments','www.faronintel.ca/deployments')
+      url = url.replace('/home/faron/lib/script/htmls/html','www.faronintel.ca')
+      url = url.replace('/home/www/html','www.faronintel.ca')
+      url = url.replace('/home/www/faronintel/html','www.faronintel.ca')
+      url = url.replace('home/faron/lib/script/nodes/projects','www.faronintel.ca/nodes')
+      url = url.replace('home/faron/bin/local/lib/node_modules','www.faronintel.ca/local')
       url = 'http://' + url
       webbrowser.open_new(url)
-
